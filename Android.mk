8cd20c0a25660fff0508601544d4eb52b36b7f3c
xyyx <xyyx@mail.ru>
2019-06-19 09:59:18 +0800

    Update makefile guard

--- Android.mk
+++ Android.mk
@@ -1,8 +1,7 @@
-ifeq ($(call my-dir),$(call project-path-for,wlan))
-
+ifeq ($(TARGET_WLAN_VARIANT),caf)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 include $(call all-subdir-makefiles)
 endif
-
 endif
+
